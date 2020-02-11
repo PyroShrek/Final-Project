31d97b43a8a24e43129f46361938fb54ea316a4b
Kristof Kaye <kristof.kay@lvjusd.k12.ca.us>
2020-02-11 14:32:55 -0800

    Started Project

---
+++ Final Project/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Final_Project.Properties.Resources.rival_pix;
+            this.pictureBox1.Location = new System.Drawing.Point(587, 476);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Final_Project.Properties.Resources.trainer_town;
+            this.pictureBox2.Location = new System.Drawing.Point(877, 716);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 71);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Final_Project.Properties.Resources.fatbone;
+            this.ClientSize = new System.Drawing.Size(1075, 955);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Final Project/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox[] myArray;
+        Random rand = new Random();
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myArray = new PictureBox[2] { pictureBox1, pictureBox2};
+            int num = rand.Next(0, myArray.Length);
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            {
+                
+                if (e.KeyCode == Keys.A)
+                {
+
+                    //move hand closer to the left  wall
+                    if (pictureBox1.Location.X > 0)
+                    {
+                        pictureBox1.Left -= 10;
+                    }
+
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    //move the insec father from left
+
+                    if (pictureBox1.Location.X < this.Width - pictureBox1.Width)
+                    {
+                        pictureBox1.Left += 10;
+                    }
+                }
+                else if (e.KeyCode == Keys.W)
+                {
+                    //move the insec father from left
+                    pictureBox1.Top -= 10;
+                }
+                else if (e.KeyCode == Keys.S)
+                {
+                    //move the insec father from left
+                    pictureBox1.Top += 10;
+                }
+                /*foreach (Label dog in myArray)
+                {
+                    if (pictureBox1.Bounds.IntersectsWith(dog.Bounds))
+                    {
+                        dog.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        dog.BackColor = Color.White;
+                    }
+                }*/
+
+                foreach (PictureBox pokemon in myArray)
+                {
+                    if (pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds))
+                    {
+                        MessageBox.Show("Let me know when your team is a little stronger.");
+                        
+                    }
+
+                }
+            }
+        }
+    }
+}
